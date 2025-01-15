772e869b996cc4e6a6824dfd3b6211b6f9cd60a0
jaesparz <jaesparz@espol.edu.ec>
2025-01-15 14:43:02 -0500

    mas tests

--- src/test/OperationsTest.java
+++ src/test/OperationsTest.java
@@ -5,6 +5,8 @@ import main.Operations;
 import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class OperationsTest extends TestCase {
 
     // Test para verificar operaciones con valores negativos
@@ -53,6 +55,51 @@ public class OperationsTest extends TestCase {
         String formula = Operations.MakeFormula();
         // Verificamos que la fórmula contiene al menos un operador
         assertTrue(formula.matches(".*[\\+\\-\\*/].*")); // La fórmula debe contener al menos un operador
+    }
+
+    // TESTS PARA SOLVE
+    @org.junit.jupiter.api.Test
+    @DisplayName("Resolver una fórmula simple de adición")
+    public void testSolveSimpleAddition() {
+        String formula = "50+30";
+        String result = Operations.Solve(formula);
+        assertEquals("50+30=80", result);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Resolver una fórmula simple de sustracción")
+    public void testSolveSimpleSubtraction() {
+        String formula = "10-4";
+        String result = Operations.Solve(formula);
+        assertEquals("10-4=6", result);
+    }
+
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Multiplicación básica de dos números")
+    public void testSimpleMultiplication() {
+        String formula = "20*30";
+        String result = Operations.Solve(formula);
+        assertEquals("20*30=600", result);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("División básica de dos números")
+    public void testSimpleDivision() {
+        String formula = "80/20";
+        String result = Operations.Solve(formula);
+        assertEquals("80/20=4", result);
+    }
+
+    @org.junit.jupiter.api.Test
+    @DisplayName("Lanza ArithmeticException cuando hay división por cero")
+    public void testDivisionByZeroThrowsArithmeticException() {
+        String formula = "5/0";
+        assertThrows(ArithmeticException.class, () -> {
+            Operations.Solve(formula);
+        });
+
+
     }
 }
 
